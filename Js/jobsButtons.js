1f810acdd673c6a221d347fb0005902ec04c74fd
Shipon Roy <shiponworks@gmail.com>
2026-02-22 12:17:05 +0600

    Completes: Available Jobs Buttons Interactives

---
+++ Js/jobsButtons.js
@@ -0,0 +1,36 @@
+const getButton = document.querySelectorAll('.btn');
+const getAllSection = document.querySelectorAll('.sections');
+
+// All Section
+function toggleStyle(id) {
+  getButton.forEach(btn => {
+    btn.classList.remove('bg-primaryColor', 'text-white');
+    btn.classList.add('text-black');
+  });
+
+  // active Button
+  const active = document.getElementById(id);
+  active.classList.remove('text-black');
+  active.classList.add('bg-primaryColor', 'text-white');
+
+  // Hidden All Section
+  getAllSection.forEach(section => {
+    section.classList.add('hidden');
+  });
+
+  // Hidden Of Counts
+  document.getElementById('interviewOfCounts').classList.add('hidden');
+  document.getElementById('rejectedOfCounts').classList.add('hidden');
+
+  // Show Section
+  if (id === 'allAvailableJobButton') {
+    document.getElementById('allSection').classList.remove('hidden');
+  } else if (id === 'interviewAvailableButton') {
+    document.getElementById('interviewSection').classList.remove('hidden');
+    document.getElementById('interviewOfCounts').classList.remove('hidden');
+    // const crateInterviewCount = document.createElement('p');
+  } else if (id === 'rejectedAvailableJobs') {
+    document.getElementById('rejectedSection').classList.remove('hidden');
+    document.getElementById('rejectedOfCounts').classList.remove('hidden');
+  }
+}
